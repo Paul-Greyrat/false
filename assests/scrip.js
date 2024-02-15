604341bbccdf959b86083f5b2d556db87d81c7e8
Nguyễn Văn Quyết <121377981+Paul1622005@users.noreply.github.com>
2024-02-15 15:03:25 +0700

    Update scrip.js

--- assests/scrip.js
+++ assests/scrip.js
@@ -2,14 +2,14 @@ const buyBtns = document.querySelectorAll('.js-buy-btn');
 const closeTicket = document.querySelector('.js-close-ticket');
 const modal = document.querySelector('.js-modal');
 const modalContainer = document.querySelector('.js-modal-container')
-//hàm thêm, xóa open
+
 function showTicket(){
      modal.classList.add('open')
 };
 function hidenTicket(){
     modal.classList.remove('open')
 };
-// câu lệnh gọi khi click
+
 for (const buyBtn of buyBtns){
     buyBtn.addEventListener('click', showTicket)
 };
